cb0c6e383c30d35ed1ee95a5d608822c85fd8b0c
Ymeis <69618260+ymmeng@users.noreply.github.com>
2022-10-06 22:13:55 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=ca531b02-ba18-4fbe-a206-fd137dde49ae
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.bing.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
